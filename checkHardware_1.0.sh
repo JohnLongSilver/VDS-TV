a13065db86d79d263af150bb04b93c1c036362a1
JohnLongSilver <laurent.orban@gmail.com>
2015-08-14 14:15:20 +0200

    add MCE check and rename it to be more generic (release)

--- checkHardware_1.0.sh
+++ checkHardware_1.0.sh
@@ -11,6 +11,7 @@ set -o pipefail
 #	v 0.1.1 27-02-2015 -- fancy it into a real script
 #	v 0.1.2 03-03-2015 -- make the ssh to server work in a loop with ip taken of a formated file
 #	v 0.1.3 04-03-2015 -- finaly output the battery status and the firmware version
+#   v 1.0   14-08-2015 -- add MCE check and rename it to be more generic (release)
 
 readonly PROGNAME=$(basename $0 .sh)
 
@@ -19,6 +20,7 @@ LOG_FILE=/arroyo/log/${PROGNAME}.log.$(date +%Y%m%d)
 SSH_CONNECTION_TIMEOUT=30
 BATTERY_REPLACEMENT_THRESHOLD=10
 VERBOSE=0
+ARGUMENTS=$@
 
 #
 # Print the command line help message.
@@ -86,19 +88,52 @@ retrieveMegaRaidStatus() {
             # prevent Ctrl-C the loop
             trap ':' INT
 
-            >/tmp/${line}.${PROGNAME}.log
+            >/tmp/${line}.${PROGNAME}.MegaRaid.log
 
             echo "retrieve battery status and firmware version for ${line}"
 
             #will later add -n to ssh in order to prevent it from reading stdin 
-            ssh -o ConnectTimeout=${SSH_CONNECTION_TIMEOUT} -o StrictHostKeyChecking=no root@${line} 'bash -s' << __EO_SSH__ > /tmp/${line}.${PROGNAME}.log
+            ssh -o ConnectTimeout=${SSH_CONNECTION_TIMEOUT} -o StrictHostKeyChecking=no root@${line} 'bash -s' << __EO_SSH__ > /tmp/${line}.${PROGNAME}.MegaRaid.log
 
             /opt/MegaRAID/CmdTool2/CmdTool2 -AdpEventLog -GetEvents -f /tmp/megaraid.log -aALL
 
             cat /tmp/megaraid.log
 __EO_SSH__
         } || {
-            rm /tmp/${line}.${PROGNAME}.log
+            rm /tmp/${line}.${PROGNAME}.MegaRaid.log
+            unset servers[${index}]
+        }
+
+    let ++index
+    done
+}
+
+#
+#
+#
+retrieveMCEStatus() {
+
+    local index=0
+
+    for line in "${@}"
+    do
+        {
+            # prevent Ctrl-C the loop
+            trap ':' INT
+
+            >/tmp/${line}.${PROGNAME}.MCE.log
+
+            echo "retrieve MCE for ${line}"
+
+            #will later add -n to ssh in order to prevent it from reading stdin
+            ssh -o ConnectTimeout=${SSH_CONNECTION_TIMEOUT} -o StrictHostKeyChecking=no root@${line} 'bash -s' << __EO_SSH__ > /tmp/${line}.${PROGNAME}.MCE.log
+
+            /usr/sbin/mcelog 2> /tmp/mcelog.log
+
+            cat /tmp/mcelog.log
+__EO_SSH__
+        } || {
+            rm /tmp/${line}.${PROGNAME}.MCE.log
             unset servers[${index}]
         }
 
@@ -117,7 +152,7 @@ showBatteryStatus() {
                                     -e "Battery Not Present" \
                                     -e "BBU removed" \
                                     -e "BBU not seen" \
-                            /tmp/${line}.${PROGNAME}.log)
+                            /tmp/${line}.${PROGNAME}.MegaRaid.log)
 
         if (("${OCCURRENCES}" >= "${BATTERY_REPLACEMENT_THRESHOLD}")); then
             echo "[${OCCURRENCES} occurences] ${line} needs to replace the MegaRaid battery!"
@@ -134,10 +169,29 @@ showFirmwareVersion () {
 
     for line in "${@}"
     do
-       local OUTPUT=$(grep -m 1 "Event Description: Firmware version" /tmp/${line}.${PROGNAME}.log | sed 's/Event Description: //')
+       local OUTPUT=$(grep -m 1 "Event Description: Firmware version" /tmp/${line}.${PROGNAME}.MegaRaid.log | sed 's/Event Description: //')
        echo "${line} ${OUTPUT}"
     done
 }
+
+#
+#
+#
+showMCEStatus () {
+
+    for line in "${@}"
+    do
+        local OUTPUT=$(grep -v "mcelog: warning: record length longer than expected. Consider update." /tmp/${line}.${PROGNAME}.MCE.log)
+        local OUTPUTSIZE=$(echo ${OUTPUT} | sed '/^$/d' | wc -l | awk '{print $1}' )
+
+        if (("${OUTPUTSIZE}" > 0 )); then
+            echo "MCE Status for ${line} ${OUTPUT}"
+        else
+            echo "MCE Status for ${line} is empty"
+        fi
+    done
+}
+
 
 #
 #
@@ -146,7 +200,7 @@ verbose() {
 
 cat << __EO_HEADER__
 ========================================================================
-Command invoked : $0 $@
+Command invoked : $0 ${ARGUMENTS}
 By user : $(whoami)
 At $(date)
 Logging to : ${LOG_FILE}
@@ -164,23 +218,31 @@ main() {
     declare -a servers=($(grep -e "^vault" -e "^streamer" ${SERVER_LIST_FILE} | awk '{print $2}'))
 
     {
-    if (( ${VERBOSE} )); then
-        verbose
-    fi
+        if (( ${VERBOSE} )); then
+            verbose
+        fi
 
-    if [ ${#servers[@]} -eq 0 ]; then
+        if [ ${#servers[@]} -eq 0 ]; then
 
-        echo "There is no Vault nor Streamer to be checked."
-        exit 0
-    else
-        echo "There is ${#servers[@]} vault/streamers to be checked."
-    fi
+            echo "There is no Vault nor Streamer to be checked."
+            exit 0
+        else
+            echo "There is ${#servers[@]} vault/streamers to be checked."
+        fi
 
-    retrieveMegaRaidStatus "${servers[@]}"
+        echo "=== retrieval ==="
 
-    showBatteryStatus "${servers[@]}"
+        retrieveMegaRaidStatus "${servers[@]}"
 
-    showFirmwareVersion "${servers[@]}"
+        retrieveMCEStatus "${servers[@]}"
+
+        echo "=== status ==="
+
+        showBatteryStatus "${servers[@]}"
+
+        showFirmwareVersion "${servers[@]}"
+
+        showMCEStatus "${servers[@]}"
 
     } | tee -a ${LOG_FILE}
 
